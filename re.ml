edfda5f27010318f2c9e79d14fb90ed2682159bb
ccQpein <ccqpein@protonmail.com>
2019-01-02 00:16:19 -0500

    finish one-line parser

--- re.ml
+++ re.ml
@@ -1,13 +1,32 @@
 #require "Str";;
 
+
 type crumb =
   |Nothing
   |Content of string
-  |Content_with_keyword of string * string;;
+  |Content_with_keyword of string * string
+;;
 
-let pick_comment_out (sym:string) ~line:(line:string) : crumb=
-  let reg = (Str.regexp (sym ^ ":=")) in
-  match Str.split reg (Str.global_replace (Str.regexp "[\r\n\t ]") "" line) with
+
+let pick_comment_out (sym:string) ~line:(line:string) : crumb =
+  let reg = (Str.regexp (sym ^ ":= *")) in
+  match Str.split reg line with
   |a::_ -> Content a
-  |_ -> Nothing;;
+  |_ -> Nothing
+;;
+
+
+let keyword_filter keyword ~content:(x:crumb) : crumb =
+match x with
+  |Content c ->
+    let keyword_reg = Str.regexp (keyword ^ ": *") in
+    if Str.string_match keyword_reg c 0
+    then Content_with_keyword ((keyword ^ ":"), List.hd (Str.split keyword_reg c))
+    else x
+  |_ -> x
+;;
+
+
+(* need json file reader *)
+(* need file reader *)
 
